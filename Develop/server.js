89e50c0f3c4beb9906c007a9409cccd4e1cd50eb
Mars <wilson.marissarose@gmail.com>
2020-11-08 14:32:40 -0500

    Added routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
+});
