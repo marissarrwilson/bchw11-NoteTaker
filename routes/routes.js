a0b35851907000b4f48da6c28ecb5d050969d6a1
Mars <wilson.marissarose@gmail.com>
2020-11-09 11:15:32 -0500

    Edits in JS files

--- routes/routes.js
+++ routes/routes.js
@@ -2,23 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
-
-     // Setup notes variable
      fs.readFile("db/db.json", "utf8", (err, data) => {
-
           if (err) throw err;
-
           var notes = JSON.parse(data);
-
-          // API ROUTES
-          // ========================================================
-
-          // Setup the /api/notes get route
           app.get("/api/notes", function (req, res) {
                res.json(notes);
           });
-
-          // Setup the /api/notes post route
           app.post("/api/notes", function (req, res) {
                // Receives a new note, adds it to db.json, then returns the new note
                let newNote = req.body;
@@ -26,29 +15,20 @@ module.exports = app => {
                updateDb();
                return console.log("Added new note: " + newNote.title);
           });
-
-          // Retrieves a note with specific id
           app.get("/api/notes/:id", function (req, res) {
                res.json(notes[req.params.id]);
           });
-
           app.delete("/api/notes/:id", function (req, res) {
                notes.splice(req.params.id, 1);
                updateDb();
                console.log("Deleted note with id " + req.params.id);
           });
-
-          // Display notes.html when /notes is accessed
           app.get('/notes', function (req, res) {
                res.sendFile(path.join(__dirname, "../public/notes.html"));
           });
-
-          // Display index.html when all other routes are accessed
           app.get('*', function (req, res) {
                res.sendFile(path.join(__dirname, "../public/index.html"));
           });
-
-          //updates the json file whenever a note is added or deleted
           function updateDb() {
                fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
                     if (err) throw err;
